6ab9906a34b33913f568f3a899d3f6b1e3aeac2a
vipulgit-123 <vipul99819@gmail.com>
2026-09-04 23:33:07 +0530

    Update

--- my-app/src/Component/News.js
+++ my-app/src/Component/News.js
@@ -53,51 +53,25 @@ export default class News extends Component {
     await this.updateNews();
   }
 
-  // handleNext = async () => {
-  //   console.log("Next");
-  //   let nextPage = this.state.page + 1;
-  //   this.setState(
-  //     {
-  //       page: nextPage,
-  //     },
-  //     () => {
-  //       this.updateNews();
-  //     },
-  //   );
-  // };
-
-  // handlePrev = async () => {
-  //   console.log("Previous");
-  //   let prevPage = this.state.page - 1;
-  //   if (prevPage < 1) {
-  //     return;
-  //   }
-  //   this.setState(
-  //     {
-  //       page: prevPage,
-  //     },
-  //     () => {
-  //       this.updateNews();
-  //     },
-  //   );
-  // };
-
   fetchMoreData = async () => {
 
-    this.setState({ page: this.state.page + 1 });
+    const nextPage = this.state.page + 1;
 
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ff0eb82f79a94cd7bbd165f7ec2d761a&page=${this.state.page}&pageSize=${this.state.pageSize}`;
     this.setState({ loading: true });
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ff0eb82f79a94cd7bbd165f7ec2d761a&page=${nextPage}&pageSize=${this.state.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(data);
-    console.log(parsedData);
 
+    console.log("Fetching page:", nextPage);
+
+    console.log(parsedData);
       this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      // articles: parsedData.articles || [],
-      totalResults: parsedData.totalResults,
+        page: nextPage,
+      articles: this.state.articles.concat(parsedData.articles || []),
+      totalResults: parsedData.totalResults || 0,
       loading: false,
     });
   };
@@ -130,9 +104,9 @@ export default class News extends Component {
         >
           <div className="container">
               <div className="row">
-            {this.state.articles.map((element) => {
+            {this.state.articles.map((element,index) => {
               return (
-                <div className="col-md-3 my-2" key={element.url}>
+                <div className="col-md-3 my-2" key={`${element.url}-${index}`}>
                   <NewsItem
                     title={(element.title || "")
                       .split(" ")
@@ -147,7 +121,7 @@ export default class News extends Component {
                     newsUrl={element.url}
                     author={element.author}
                     date={element.publishedAt}
-                    source={element.source.name}
+                    source={element.source?.name}
                   />
                 </div>
               );
@@ -155,52 +129,6 @@ export default class News extends Component {
           </div>
           </div>
         </InfiniteScroll>
-        {/*<div className="row">*/}
-        {/*  {!this.state.loading &&*/}
-        {/*    this.state.articles.map((element) => {*/}
-        {/*      return (*/}
-        {/*        <div className="col-md-3 my-2" key={element.url}>*/}
-        {/*          <NewsItem*/}
-        {/*            title={(element.title || "")*/}
-        {/*              .split(" ")*/}
-        {/*              .slice(0, 5)*/}
-        {/*              .join(" ")}*/}
-        {/*            description={*/}
-        {/*              element.description*/}
-        {/*                ? element.description.split(" ").slice(0, 15).join(" ")*/}
-        {/*                : element.title*/}
-        {/*            }*/}
-        {/*            imageUrl={element.urlToImage}*/}
-        {/*            newsUrl={element.url}*/}
-        {/*            author={element.author}*/}
-        {/*            date={element.publishedAt}*/}
-        {/*            source={element.source.name}*/}
-        {/*          />*/}
-        {/*        </div>*/}
-        {/*      );*/}
-        {/*    })}*/}
-        {/*</div>*/}
-        {/*<div className="container d-flex justify-content-between">*/}
-        {/*  <button*/}
-        {/*    disabled={this.state.page <= 1}*/}
-        {/*    type="button"*/}
-        {/*    className="btn btn-light"*/}
-        {/*    onClick={this.handlePrev}*/}
-        {/*  >*/}
-        {/*    <strong>&larr; Previous</strong>*/}
-        {/*  </button>*/}
-        {/*  <button*/}
-        {/*    disabled={*/}
-        {/*      this.state.page >=*/}
-        {/*      Math.ceil(this.state.totalResults / this.state.pageSize)*/}
-        {/*    }*/}
-        {/*    type="button"*/}
-        {/*    className="btn btn-light"*/}
-        {/*    onClick={this.handleNext}*/}
-        {/*  >*/}
-        {/*    <strong>Next &rarr;</strong>*/}
-        {/*  </button>*/}
-        {/*</div>*/}
       </div>
     );
   }
